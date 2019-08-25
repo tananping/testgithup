3eb728dfa0f44413032955149f286a90ebac55b2
tap <3249899068@qq.com>
2019-08-25 22:49:52 +0800

    新建项目

---
+++ src/main/java/com/yc/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.yc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author an-ping
+ * @date 2019/8/25
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(value="/say.action")
+    public String say(){
+        return "hello world";
+    }
+}
